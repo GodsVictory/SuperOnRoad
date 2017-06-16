3d8ccb1b7bebd3a04b841c5c3ba8e6363b4c2ad5
GodsVictory <thegodsvictory@gmail.com>
2017-06-16 13:36:07 -0500

    formatting

--- public/js/game.js
+++ public/js/game.js
@@ -8,20 +8,20 @@ var tileSize = 5;
 var players = {};
 
 window.onload = function start() {
-    openSocket();
+	openSocket();
 
-    PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
+	PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
-    if (isMobile.any)
-        setupMobile();
+	if (isMobile.any)
+		setupMobile();
 
-    app = new PIXI.Application(800, 600, {
-        antialias: true
-    });
-    document.body.appendChild(app.view);
-    app.renderer.view.style.position = 'absolute';
-    app.renderer.view.style.top = 0;
-    app.renderer.view.style.left = 0;
+	app = new PIXI.Application(800, 600, {
+		antialias: true
+	});
+	document.body.appendChild(app.view);
+	app.renderer.view.style.position = 'absolute';
+	app.renderer.view.style.top = 0;
+	app.renderer.view.style.left = 0;
 
-    setupInput();
+	setupInput();
 }
