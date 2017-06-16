f5c63436efcbf5c429f74ea88cc1a569fe719683
GodsVictory <thegodsvictory@gmail.com>
2017-06-15 23:40:25 -0500

    fix mobile layout

--- public/js/setupMobile.js
+++ public/js/setupMobile.js
@@ -9,11 +9,7 @@ function setupMobile() {
 	mobile.renderer.view.style.top = 600;
 	mobile.renderer.view.style.left = 0;
 
-	var width = mobile.view.width;
-	var height = mobile.view.height;
-	var arrowSize = 150;
-
-	var arrowUp = new Arrow(arrowSize, 150, 0);
+	var arrowUp = new Arrow(150, 100, 0);
 	arrowUp.arrow.on('pointerdown', function(e) {
 		player.pressForward();
 	});
@@ -21,7 +17,7 @@ function setupMobile() {
 		player.releaseForward();
 	});
 
-	var arrowDown = new Arrow(arrowSize, 350, 1);
+	var arrowDown = new Arrow(150, 300, 1);
 	arrowDown.arrow.on('pointerdown', function(e) {
 		player.pressBack();
 	});
@@ -29,7 +25,7 @@ function setupMobile() {
 		player.releaseBack();
 	});
 
-	var arrowLeft = new Arrow(width - arrowSize * 2, 350, 1.5);
+	var arrowLeft = new Arrow(500, 300, 1.5);
 	arrowLeft.arrow.on('pointerdown', function(e) {
 		player.pressLeft();
 	});
@@ -37,7 +33,7 @@ function setupMobile() {
 		player.releaseLeft();
 	});
 
-	var arrowRight = new Arrow(width - arrowSize * .75, 350, .5);
+	var arrowRight = new Arrow(700, 300, .5);
 	arrowRight.arrow.on('pointerdown', function(e) {
 		player.pressRight();
 	});
@@ -45,15 +41,15 @@ function setupMobile() {
 		player.releaseRight();
 	});
 
-	var boost = new Arrow(width - arrowSize * 1.375, 150, 0);
+	var boost = new Arrow(600, 100, 0);
 	boost.arrow.on('pointerdown', function(e) {
 		player.boost();
 	});
 
 	function Arrow(x, y, r) {
 		this.arrow = PIXI.Sprite.fromImage('assets/interface/arrow.png');
-		this.arrow.width = arrowSize;
-		this.arrow.height = arrowSize;
+		this.arrow.width = 150;
+		this.arrow.height = 150;
 		this.arrow.anchor.set(0.5);
 		this.arrow.rotation = Math.PI * r;
 		this.arrow.x = x;
