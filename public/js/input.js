7e3cbf3294972f9f6250a6b882315a457c5cbde4
GodsVictory <thegodsvictory@gmail.com>
2017-06-22 11:43:58 -0500

    fix rubber banding

--- public/js/input.js
+++ public/js/input.js
@@ -27,45 +27,53 @@ function setupInput() {
 
 function pressForward() {
   // socket.emit('key', 'pressForward');
-  player.input('pressForward');
+  player.forward = 1;
+  // player.input('pressForward');
 }
 
 function releaseForward() {
   // socket.emit('key', 'releaseForward');
+  player.forward = 0;
   player.input('releaseForward');
 }
 
 function pressBack() {
   // socket.emit('key', 'pressBack');
+  player.back = 1;
   player.input('pressBack');
 }
 
 function releaseBack() {
   // socket.emit('key', 'releaseBack');
+  player.back = 0;
   player.input('releaseBack');
 }
 
 function pressLeft() {
   // socket.emit('key', 'pressLeft');
+  player.left = 1;
   player.input('pressLeft');
 }
 
 function releaseLeft() {
   // socket.emit('key', 'releaseLeft');
+  player.left = 0;
   player.input('releaseLeft');
 }
 
 function pressRight() {
   // socket.emit('key', 'pressRight');
+  player.right = 1;
   player.input('pressRight');
 }
 
 function releaseRight() {
   // socket.emit('key', 'releaseRight');
+  player.right = 0;
   player.input('releaseRight');
 }
 
 function boost() {
   // socket.emit('key', 'boost');
-  player.input('boost');
+  player.boost();
 }
--- public/js/player.js
+++ public/js/player.js
@@ -18,26 +18,6 @@ function Player(x, y, rotation, speed, turn, type, levelBounds) {
   this.boostCooldown = 3000;
   this.boostStart = 0;
   this.boostEnd = 0;
-  this.input = function(data) {
-    if (data == 'pressForward')
-      this.forward = 1;
-    if (data == 'releaseForward')
-      this.forward = 0;
-    if (data == 'pressBack')
-      this.back = 1;
-    if (data == 'releaseBack')
-      this.back = 0;
-    if (data == 'pressLeft')
-      this.left = 1;
-    if (data == 'releaseLeft')
-      this.left = 0;
-    if (data == 'pressRight')
-      this.right = 1;
-    if (data == 'releaseRight')
-      this.right = 0;
-    if (data == 'boost')
-      this.boost();
-  }
   this.boost = function() {
     if (this.boostStart == 0)
       if (this.boostEnd == 0 || Date.now() - this.boostEnd >= this.boostCooldown) {
@@ -45,7 +25,6 @@ function Player(x, y, rotation, speed, turn, type, levelBounds) {
         this.boostStart = Date.now();
       }
   }
-
   this.update = function(delta) {
     if (player.truck.update) {
       player.truck.setPos(player.truck.update.x, player.truck.update.y);
@@ -66,15 +45,16 @@ function Player(x, y, rotation, speed, turn, type, levelBounds) {
     updates.push(update);
     socket.emit('input', update);
     this.updatePos(update);
+    this.truck.setPos(this.x, this.y);
+    this.truck.setRotation(this.rotation);
   }
-
   this.updatePos = function(data) {
-
-    this.truck.setRotation(this.truck.getRotation() + (-data.left + data.right) * this.turn * data.delta);
-
+    this.rotation = this.truck.getRotation() + (-data.left + data.right) * this.turn * data.delta;
     var x = this.truck.getX() + (data.forward - data.back) * this.speed * Math.sin(this.truck.getRotation()) * data.delta;
     var y = this.truck.getY() - (data.forward - data.back) * this.speed * Math.cos(this.truck.getRotation()) * data.delta;
-    if (this.levelBounds[Math.round(x / tileSize) + "," + Math.round(y / tileSize)])
-      this.truck.setPos(x, y);
+    if (this.levelBounds[Math.round(x / tileSize) + "," + Math.round(y / tileSize)]) {
+      this.x = x;
+      this.y = y;
+    }
   }
 }
