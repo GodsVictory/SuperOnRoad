171c49a428273cd4350f4c9e2ae4f96066c52f34
GodsVictory <thegodsvictory@gmail.com>
2017-06-23 01:26:18 -0500

    test

--- public/js/player.js
+++ public/js/player.js
@@ -24,9 +24,9 @@ function Player(x, y, rotation, speed, turn, type, levelBounds) {
       this.x = updateData.x;
       this.y = updateData.y;
       this.rotation = updateData.rotation;
-      for (var i = updateData.seq; i < this.seq; i++) {
-        console.log('test');
-        console.log('asdf');
+      var len = this.seq;
+      for (var i = updateData.seq; i < len; i++) {
+        console.log(this.updates[i].forward);
         player.updatePos(this.updates[i]);
       }
     }
